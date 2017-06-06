ca84e6996d8f101ffe68a011e553827b0fccce2c
matteskr <matteskr@mcmaster.ca>
2017-06-06 11:46:55 -0400

    Delete driver.c

--- driver.c
+++
@@ -1,9 +0,0 @@
-#include "cdecl.h"
-
-int PRE_CDECL asm_main(int,char**) POST_CDECL;
-
-int main(int argc,char** argv) {
-  int ret_status;
-  ret_status = asm_main(argc,argv);
-  return ret_status;
-}
